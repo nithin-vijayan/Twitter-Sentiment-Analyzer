b8cb7cfd0aeb39337c8c96572c4f652ec72f9ab9
Nithin K <nithinkvijayan@icloud.com>
2018-06-06 13:59:35 +0530

    Updated script to filter out retweets

--- twitterAnalyzer.py
+++ twitterAnalyzer.py
@@ -20,14 +20,17 @@ class Twitter(object):
 	def get_tweets(self, query, count, lang):
 		twitterFeed = {}
 		try:
-			twitterFeed = self.api.search(q=query, lang=lang, count=count, )
+			twitterFeed = self.api.search(q=query+" -filter:retweets", lang=lang, count=count, ) #Filter out retweets and query for the new feeds
 		except TwythonAuthError:
 			print "Authentication with the api failed"
 		return twitterFeed
 
 	def clean_tweets(self, twitterFeed):		
 		tweetsRaw = twitterFeed["statuses"]
-		tweets = [ eachTweet["text"] for eachTweet in tweetsRaw ]
+		tweets = []
+		for eachTweet in tweetsRaw:
+			if not eachTweet["retweeted"] and ('RT @' not in eachTweet["text"]): #Filter any retweets, if any ,which still managed to show up
+				tweets.append(eachTweet["text"])
 		return tweets
 
 class SentimentAnalyzer(object):
